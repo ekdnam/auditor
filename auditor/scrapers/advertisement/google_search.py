7a7ae2b831f02396247cb1d9201d118a86f9ea4f
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2022-12-19 22:58:40 -0600

    j

--- auditor/scrapers/advertisement/google_search.py
+++ auditor/scrapers/advertisement/google_search.py
@@ -8,6 +8,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 
+# from Screenshot import Screenshot_Clipping
+
 from auditor.agent import Agent
 from auditor.scrapers.advertisement.base_ad_scrapers import BaseAdScraper
 from auditor.scrapers.base_scraper import strip_html_tags
@@ -23,6 +25,16 @@ class GoogleSearchAdScraper(BaseAdScraper):
         super().__init__(query, delay, pages)
 
     def __call__(self, unit: Agent, queue: Queue):
+        def save_screenshot(driver, path: str = '/tmp/screenshot.png') -> None:
+            # Ref: https://stackoverflow.com/a/52572919/
+            original_size = driver.get_window_size()
+            required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+            required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+            driver.set_window_size(required_width, required_height)
+            # driver.save_screenshot(path)  # has scrollbar
+            driver.find_element_by_tag_name('body').screenshot(path)  # avoids scrollbar
+            driver.set_window_size(original_size['width'], original_size['height'])
+
         with GoogleSearchAdScraper.scrape_lock:
             driver = unit.driver
             time.sleep(self.delay)
@@ -36,28 +48,33 @@ class GoogleSearchAdScraper(BaseAdScraper):
                     .perform()
             except NoSuchElementException:
                 self.logger.exception("Unexpected exception")
-
+            i = 0
             for page in range(self.pages):
                 time.sleep(self.delay)
                 if "https://www.google.com/sorry/index" in driver.current_url:
                     self.logger.error("Hit anti-bot captcha on page %i", page)
                     return
-
+                # ss = Screenshot_Clipping.Screenshot()
+                # image = ss.full_Screenshot(driver, save_path=r'.' , image_name=f'{i}.png')
+                self.logger.info('Taking screenoshot')
+                save_screenshot(driver, path=f'{i}.png')
+                i += 1
                 ads = driver.find_elements_by_css_selector("li.ads-ad")
-                for ad in ads:
-                    try:
-                        title = ad.find_element_by_css_selector('div.ad_cclk a:not([style*="display:none"])').text
-                        body = strip_html_tags(str(ad.find_element_by_css_selector("div.ads-creative")
-                                                   .get_attribute('innerHTML')))
-                        parsed_ad = dict()
-                        parsed_ad['query'] = self.query
-                        parsed_ad['title'] = strip_html_tags(title)
-                        parsed_ad['url'] = ad.find_element_by_css_selector("div.ads-visurl cite").text
-                        parsed_ad['body'] = strip_html_tags(body)
-                        self.log_scraped_ad(queue, unit, self, parsed_ad)
-                        self.logger.debug("Complete ad: %s", ad.get_attribute('outerHTML'))
-                    except NoSuchElementException:
-                        self.logger.exception("Unparseable ad: %s", ad.get_attribute('outerHTML'))
+                # for ad in ads:
+                #     try:
+                #         title = ad.find_element_by_css_selector('div.ad_cclk a:not([style*="display:none"])').text
+                #         body = strip_html_tags(str(ad.find_element_by_css_selector("div.ads-creative")
+                #                                    .get_attribute('innerHTML')))
+                #         parsed_ad = dict()
+                #         parsed_ad['query'] = self.query
+                #         parsed_ad['title'] = strip_html_tags(title)
+                #         parsed_ad['url'] = ad.find_element_by_css_selector("div.ads-visurl cite").text
+                #         parsed_ad['body'] = strip_html_tags(body)
+                #         self.log_scraped_ad(queue, unit, self, parsed_ad)
+                #         self.logger.debug("Complete ad: %s", ad.get_attribute('outerHTML'))
+                #         driver.save_screenshot('ss.png')
+                #     except NoSuchElementException:
+                #         self.logger.exception("Unparseable ad: %s", ad.get_attribute('outerHTML'))
                 try:
                     driver.execute_script("arguments[0].scrollIntoView();",
                                           driver.find_element_by_css_selector("#pnnext"))
