b327172e96edb5d65464c9bc5d77d256bc04e837
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2023-02-19 12:26:53 -0800

    easier setting up of vm

---
+++ vm-setup.sh
@@ -0,0 +1,22 @@
+# initial setup of vm 
+sudo apt-get update
+sudo apt-get upgrade
+
+# install python
+sudo add-apt-repository ppa:deadsnakes/ppa 
+sudo apt-get update 
+sudo apt-get install python3.7
+sudo apt install python3-pip
+
+# install firefox and firefox driver
+wget https://github.com/mozilla/geckodriver/releases/download/v0.32.2/geckodriver-v0.32.2-linux64.tar.gz
+tar -C /usr/local/bin/ -xvf geckodriver-v0.32.2-linux64.tar.gz
+sudo apt install firefox
+
+# depedencies for imagehash
+sudo apt install libjpeg-dev zlib1g-dev
+# install xvfb
+sudo apt-get install xvfb
+
+# install depedencies
+pip install --editable . -r requirements.txt
