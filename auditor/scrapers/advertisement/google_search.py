9c4d9b01b774b87d54549e8a1a70de241fc5cf5c
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2023-04-17 16:51:31 -0700

    see what is going wrong

--- auditor/scrapers/advertisement/google_search.py
+++ auditor/scrapers/advertisement/google_search.py
@@ -19,6 +19,15 @@ from auditor.scrapers.base_scraper import strip_html_tags
 
 INPUT_SELECTOR = "input[title='Search']"
 
+import requests
+
+def connected_to_internet(url='http://www.google.com/', timeout=5):
+    try:
+        _ = requests.head(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        print("No internet connection available.")
+    return False
 
 class GoogleSearchAdScraper(BaseAdScraper):
     logger = logging.getLogger(__name__)
@@ -29,18 +38,9 @@ class GoogleSearchAdScraper(BaseAdScraper):
         self.name = 'GoogleSearchAdScraper'
 
     def __call__(self, unit: Agent, queue: Queue):
-        def save_screenshot(driver, path: str = '/tmp/screenshot.png') -> None:
-            # Ref: https://stackoverflow.com/a/52572919/
-            original_size = driver.get_window_size()
-            required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
-            required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-            driver.set_window_size(required_width, required_height)
-            # driver.save_screenshot(path)  # has scrollbar
-            driver.find_element_by_tag_name('body').screenshot(path)  # avoids scrollbar
-            driver.set_window_size(original_size['width'], original_size['height'])
-
-        def save_file(driver, folder, filename):
+        def save_file(unit, driver, folder, filename):
             # Ref: https://www.tutorialspoint.com/save-a-web-page-with-python-selenium
+            self.logger.info("Saving file ---  ", unit.treatment_id)
             save_path = f'{os.getcwd()}/{folder}'
             if not os.path.exists(save_path):
                 os.mkdir(os.path.join(os.getcwd(), folder))
@@ -49,66 +49,33 @@ class GoogleSearchAdScraper(BaseAdScraper):
             h = driver.page_source
             f.write(h)
 
-        with GoogleSearchAdScraper.scrape_lock:
-            driver = unit.driver
+        # with GoogleSearchAdScraper.scrape_lock:
+        driver = unit.driver
+        time.sleep(self.delay)
+        try:
+            driver.get("https://www.google.com/")
+            search_bar = driver.find_element_by_css_selector(INPUT_SELECTOR)
+            ActionChains(driver) \
+                .pause(1) \
+                .send_keys_to_element(search_bar, self.query) \
+                .send_keys_to_element(search_bar, Keys.RETURN) \
+                .perform()
+        except Exception as e:
+            self.logger.info("Exception occurred while searching on google for results")
+            self.logger.exception(e)
+        i = 0
+        for page in range(self.pages):
             time.sleep(self.delay)
-            try:
-                driver.get("https://www.google.com/")
-                search_bar = driver.find_element_by_css_selector(INPUT_SELECTOR)
-                ActionChains(driver) \
-                    .pause(1) \
-                    .send_keys_to_element(search_bar, self.query) \
-                    .send_keys_to_element(search_bar, Keys.RETURN) \
-                    .perform()
-            except NoSuchElementException:
-                self.logger.exception("Unexpected exception")
-            i = 0
-            for page in range(self.pages):
-                time.sleep(self.delay)
-                if "https://www.google.com/sorry/index" in driver.current_url:
-                    self.logger.error("Hit anti-bot captcha on page %i", page)
-                    return
-                # ss = Screenshot_Clipping.Screenshot()
-                # image = ss.full_Screenshot(driver, save_path=r'.' , image_name=f'{i}.png')
-
-
-                # query file names to build data structure
-                # ref: https://stackoverflow.com/questions/10607688/how-to-create-a-file-name-with-the-current-date-time-in-python
-                # https://stackoverflow.com/questions/10501247/best-way-to-generate-random-file-names-in-python 
-                time_stamp = time.strftime("%Y%m%d-%H%M%S")
-                query_ID = str(uuid.uuid4())
-                agent_ID = str(unit.agent_id) + "___" + str(unit.treatment_id)
-                file_name = agent_ID + "___" + time_stamp + "___" + query_ID
-                # self.logger.info(unit.agent_id)
-                # self.logger.info(unit.treatment_id)
-                self.logger.info('Save file')
-                save_file(driver, 'output-html', file_name)
-                i += 1
-                ads = driver.find_elements_by_css_selector("li.ads-ad")
-
-
-
-
-                # for ad in ads:
-                #     try:
-                #         title = ad.find_element_by_css_selector('div.ad_cclk a:not([style*="display:none"])').text
-                #         body = strip_html_tags(str(ad.find_element_by_css_selector("div.ads-creative")
-                #                                    .get_attribute('innerHTML')))
-                #         parsed_ad = dict()
-                #         parsed_ad['query'] = self.query
-                #         parsed_ad['title'] = strip_html_tags(title)
-                #         parsed_ad['url'] = ad.find_element_by_css_selector("div.ads-visurl cite").text
-                #         parsed_ad['body'] = strip_html_tags(body)
-                #         self.log_scraped_ad(queue, unit, self, parsed_ad)
-                #         self.logger.debug("Complete ad: %s", ad.get_attribute('outerHTML'))
-                #         driver.save_screenshot('ss.png')
-                #     except NoSuchElementException:
-                #         self.logger.exception("Unparseable ad: %s", ad.get_attribute('outerHTML'))
-                # try:
-                #     driver.execute_script("arguments[0].scrollIntoView();",
-                #                           driver.find_element_by_css_selector("#pnnext"))
-                #     driver.find_element_by_css_selector("#pnnext").click()
-                # except NoSuchElementException:
-                #     filename = str(datetime.now()) + '.screenie.png'
-                #     driver.save_screenshot(filename)
-                #     self.logger.exception("Element not found: %s", filename)
+            if "https://www.google.com/sorry/index" in driver.current_url:
+                self.logger.error("Hit anti-bot captcha on page %i", page)
+                return
+            # query file names to build data structure
+            # ref: https://stackoverflow.com/questions/10607688/how-to-create-a-file-name-with-the-current-date-time-in-python
+            # https://stackoverflow.com/questions/10501247/best-way-to-generate-random-file-names-in-python 
+            time_stamp = time.strftime("%Y%m%d-%H%M%S")
+            query_ID = str(uuid.uuid4())
+            agent_ID = str(unit.agent_id) + "___" + str(unit.treatment_id)
+            file_name = agent_ID + "___" + time_stamp + "___" + query_ID
+            self.logger.info('Save file')
+            save_file(unit, driver, 'output-html', file_name)
+            i += 1
